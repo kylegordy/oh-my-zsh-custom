979702f5f7679c4a5a145ca91a5f582806a99fc9
kylegordy <kyle@kylegordydesign.com>
2014-06-16 16:37:10 -0400

    added simple python server alias

--- custom.zsh
+++ custom.zsh
@@ -36,6 +36,9 @@ alias hide-h="defaults write com.apple.finder AppleShowAllFiles false && killall
 # Copy lorem ipsum
 alias lorem-ipsum="echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus quis libero id mollis. Proin ultricies sit amet dui eu facilisis. Ut est odio, commodo ut volutpat sed, posuere vitae sapien. Morbi aliquet orci quis sem laoreet sodales. Donec a ultrices nisi. Proin ut purus malesuada, pellentesque lorem nec, rutrum augue. Nam congue interdum tortor id aliquet. Maecenas accumsan, neque eget dignissim consectetur, metus justo aliquam tellus, ac viverra nisl nisl et quam. Aenean at elit eu dui bibendum auctor. Quisque condimentum risus malesuada turpis interdum, nec mattis erat pellentesque. Phasellus orci diam, vulputate ut lectus eu, sollicitudin tincidunt nibh. Fusce eu libero pulvinar, dictum diam at, rutrum sapien. Integer eleifend, leo nec venenatis porttitor, massa nulla sagittis ante, ut accumsan nisl mauris ut est. Nulla facilisi. | pbcopy"
 
+# Start Simple Python Server
+alias simple-server="python -m SimpleHTTPServer"
+
 # --------------------------------------------------
 # RVM
 # --------------------------------------------------
